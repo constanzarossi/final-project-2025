6a71628a4bb342875a15f4cd4b6a05946719e333
amenon04 <am4347@georgetown.edu>
2025-12-16 17:44:59 -0500

    DONE

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 from web_app.routes.drinks_routes import drinks_routes
 
 def create_app():
     app = Flask(__name__)
+
+    # REQUIRED for flash messages 
+    app.secret_key = os.getenv("SECRET_KEY", "dev-secret-key")
+
     app.register_blueprint(drinks_routes)
     return app
